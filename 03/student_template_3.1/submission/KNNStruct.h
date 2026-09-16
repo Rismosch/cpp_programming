e7f3bded023ea794fcb93f900877eaf7a0a42077
Eric Berger <berger.eric24@gmail.com>
2026-09-16 13:37:09 +0200

    sheet 3 solution

--- 03/student_template_3.1/submission/KNNStruct.h
+++ 03/student_template_3.1/submission/KNNStruct.h
@@ -1,3 +1,5 @@
+// Simon Sutoris 7542170
+// Eric Berger 7064584
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -11,7 +13,13 @@ struct DYNPoint {
 	[[nodiscard]] static DYNPoint createRandomPoint(unsigned int size, int minimum=-5000, int maximum=5000) {
 		DYNPoint p;
 		if (size > 0 && minimum <= maximum) {			
-			// STUDENT TODO: add your code			
+			// STUDENT TODO: add your code	
+			p.data.reserve(size);
+			int range = maximum - minimum;
+			for (unsigned int i=0; i<size; i++)
+			{
+				p.data.push_back((std::rand() % (range + 1)) + minimum);
+			}
 		}
 		return p;
 	}
@@ -38,36 +46,89 @@ struct KNN {
 		int class_label = -1;
 
 		if (k && function_ptr_Distance && trainingData.size()) {
-
 			// STUDENT TODO: your code
+			size_t dataset_size = trainingData.size();
+			std::vector<unsigned int> labels(k);
+			std::vector<float> distances(dataset_size);
 
+			for (size_t i=0; i<dataset_size; i++)
+			{
+				distances.at(i) = function_ptr_Distance(trainingData.at(i).first, A);
+			}
+
+			for (unsigned int K=0; K<k; K++)
+			{
+				float min_dist = std::numeric_limits<float>::max();
+				size_t min_dist_idx = 0;
+				for (size_t i=0; i<dataset_size; i++)
+				{
+					if (distances.at(i) < min_dist)
+					{
+						min_dist = distances.at(i);
+						min_dist_idx = i;
+					}
+				}
+				labels.at(K) = trainingData.at(min_dist_idx).second;
+				distances.at(min_dist_idx) = std::numeric_limits<float>::max();
+			}
+
+			unsigned int best_count = 0;
+			for (size_t i=0; i<labels.size(); i++)
+			{
+				unsigned int count = 0;
+				for (size_t j=0; j<labels.size(); j++)
+				{
+					if (labels.at(j) == labels.at(i))
+					{
+						count += 1;
+					}
+				}
+				if (count > best_count)
+				{
+					best_count = count;
+					class_label = labels.at(i);
+				}
+			}
 		}
 		return class_label;
 	}
 };
 
 float DistanceManhattan(const DYNPoint &A, const DYNPoint &B) {
-
 	// STUDENT TODO: your code
-
-	return 0;
-
+	size_t size = A.data.size();
+	float sum = 0.0f;
+	for (size_t i=0; i<size; i++)
+	{
+		sum += std::abs(A.data.at(i) - B.data.at(i));
+	}
+	return sum / size;
 }
 
 float DistanceEuclid(const DYNPoint &A, const DYNPoint &B) {
-
 	// STUDENT TODO: your code
-
-	return 0;
+	size_t size = A.data.size();
+	float sum = 0.0f;
+	for (size_t i=0; i<size; i++)
+	{
+		float difference = A.data.at(i) - B.data.at(i);
+		sum += difference * difference;
+	}
+	return std::sqrt(sum / size);
 
 }
 
 void createDataset(std::vector<std::pair<DYNPoint, unsigned int>> &dataset, const unsigned int amount, const unsigned int class_label,
 				const unsigned int point_size, const int minimum, const int maximum) {
-
+	
 	if (amount > 0 && point_size>0 && minimum <= maximum) {
-
 	// STUDENT TODO: your code
+	dataset.reserve(dataset.size() + amount);
+	for (unsigned int i=0; i<amount; i++)
+	{
+		DYNPoint point = DYNPoint::createRandomPoint(point_size, minimum, maximum);
+        dataset.push_back({point, class_label});
+	}
 
 	}
 }
