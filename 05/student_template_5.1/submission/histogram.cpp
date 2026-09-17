9eeb49c405312f7fc823230c6d84e6dc05c0b79b
Rismosch Desktop <simon.sutoris@googlemail.com>
2026-09-17 10:38:09 +0200

    Solve 5.1 a)

--- 05/student_template_5.1/submission/histogram.cpp
+++ 05/student_template_5.1/submission/histogram.cpp
@@ -10,23 +10,32 @@
 #include <vector>
 
 Histogram::Histogram(const std::vector<std::string> &words) {
-  // TODO 5.1.a) make sure the histogram is normalized when the constructor is
-  // done
+  std::map<const std::string, size_t> count;
+  for (auto word : words) {
+    count[word] += 1;
+  }
+
+  for (auto it = count.begin(); it != count.end(); ++it) {
+    auto word = it->first;
+    auto count = it->second;
+    histogram[word] =
+        static_cast<double>(count) / static_cast<double>(words.size());
+  }
 }
 
-size_t Histogram::size() const {
-  // TODO 5.1.a)
-  return 0;
-}
+size_t Histogram::size() const { return histogram.size(); }
 
 bool Histogram::contains(const std::string &word) const {
-  // TODO 5.1.a)
-  return false;
+  return histogram.find(word) != histogram.end();
 }
 
 double Histogram::probability(const std::string &word) const {
-  // TODO 5.1.a)
-  return 0;
+  auto entry = histogram.find(word);
+  if (entry == histogram.end()) {
+    return 0.0;
+  } else {
+    return entry->second;
+  }
 }
 
 // TODO 5.1.b
