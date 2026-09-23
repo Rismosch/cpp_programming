f0b5efd68819ec342f7b7dc26424e8f3040c3c9a
Simons Thinkpad <simon.sutoris@googlemail.com>
2026-09-23 10:46:04 +0200

    Format comments

--- 09/student_template_9.1/submission/exercise_09_1.cpp
+++ 09/student_template_9.1/submission/exercise_09_1.cpp
@@ -21,13 +21,18 @@ enum class Item { BOOK = 0, TOY = 1 };
  *
  * @return Item The item that should be sold.
  *
- * @thread_safety This function is fully threadsafe. No modifications were necessary. It only uses variable local to it's thread. It does not share memory, not even globally. Therefore, this method is safe to call in any thread without any synchronisation mechanisms.
+ * @thread_safety This function is fully threadsafe. No modifications were
+ * necessary. It only uses variable local to it's thread. It does not share
+ * memory, not even globally. Therefore, this method is safe to call in any
+ * thread without any synchronisation mechanisms.
  */
 Item choose_what_to_sell() {
-  static thread_local std::default_random_engine generator; // only local to this thread
+  static thread_local std::default_random_engine generator; // only local to
+                                                            // this thread
   std::discrete_distribution<int> distribution({5, 1}); // local variable
   int choice = distribution(generator); // local variable
-  return static_cast<Item>(choice);     // cast and return copy of the local variable
+  return static_cast<Item>(choice);     // cast and return copy of the local
+                                        // variable
 }
 
 // please use these three mutexes to protect access to shared variables
@@ -38,7 +43,9 @@ std::mutex toy_mutex;
 /**
  * @brief Sell a book
  *
- * @thread_safety This function is now full threadsafe. A mutex was added, to protect read-modify-write operations on shared memory. Due to the mutex, read-writes are now done synchronously, avoiding data races.
+ * @thread_safety This function is now full threadsafe. A mutex was added, to
+ * protect read-modify-write operations on shared memory. Due to the mutex,
+ * read-writes are now done synchronously, avoiding data races.
  */
 void sell_book(Shop &shop) {
   const std::lock_guard<std::mutex> lock(item_mutex); // lock
@@ -57,7 +64,9 @@ void sell_book(Shop &shop) {
 /**
  * @brief Sell a toy
  *
- * @thread_safety This function is now full threadsafe. A mutex was added, to protect read-modify-write operations on shared memory. Due to the mutex, read-writes are now done synchronously, avoiding data races.
+ * @thread_safety This function is now full threadsafe. A mutex was added, to
+ * protect read-modify-write operations on shared memory. Due to the mutex,
+ * read-writes are now done synchronously, avoiding data races.
  */
 void sell_toy(Shop &shop) {
   const std::lock_guard<std::mutex> lock(item_mutex); // lock
@@ -77,14 +86,19 @@ void sell_toy(Shop &shop) {
  * @brief A salesperson keeps selling items.
  * When the store runs out of items she gets frustrated and quits.
  *
- * @thread_safety This function is now full threadsafe. On each loop, it only sells one item. Each operation (sell_toy, or sell_book) is already threadsafe, thus each iteration of the loop is threadsafe as well.
+ * @thread_safety This function is now full threadsafe. On each loop, it only
+ * sells one item. Each operation (sell_toy, or sell_book) is already
+ * threadsafe, thus each iteration of the loop is threadsafe as well.
  */
 void sell_stuff(Shop &shop) {
   while (true) {
     // randomly select what to sell
     try {
-      //const std::lock_guard<std::mutex> lock(item_mutex); // lock is not required, because all following operations are threadsafe already
-      if (choose_what_to_sell() == Item::TOY) { // threadsafe, generate random choice
+      // lock is not required, because all following operations are threadsafe
+      // already
+      //const std::lock_guard<std::mutex> lock(item_mutex);
+      if (choose_what_to_sell() == Item::TOY) { // threadsafe, generate random
+                                                // choice
         sell_toy(shop); // threadsafe
       } else {
         sell_book(shop); // threadsafe
