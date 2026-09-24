d1761cfbe687419871293168c1e15bc90369771e
Simons Thinkpad <simon.sutoris@googlemail.com>
2026-09-24 11:56:23 +0200

    Solve 11.1 a) and c)

--- 11/student_template_11.1/submission/shortest_paths.cpp
+++ 11/student_template_11.1/submission/shortest_paths.cpp
@@ -1,7 +1,9 @@
 #include "shortest_paths.h"
 #include <algorithm>
 #include <cmath>
+#include <cstddef>
 #include <iostream>
+#include <limits>
 #include <utility>
 
 size_t ShortestPaths::getNodeIdByName(const std::string &name) const {
@@ -16,15 +18,78 @@ size_t ShortestPaths::getNodeIdByName(const std::string &name) const {
 }
 
 std::vector<size_t> ShortestPaths::compute_shortest_path(size_t from,
-                                                         size_t to) const {
-  /// your result path
-  std::vector<size_t> result;
-  /// increment this for every node that you pop from the queue
-  size_t num_visited = 0;
+                                                     size_t to) const {
+  // unkown distance function from source to v
+  std::vector<float> dist(
+          adjacency_matrix.size(),
+          std::numeric_limits<float>::infinity()
+  );
 
-  // TODO: your code here
+  // previous node in optimal path from source
+  const size_t undefined = ~static_cast<size_t>(0);
+  std::vector<size_t> previous(
+    adjacency_matrix.size(),
+    undefined
+  );
 
-  std::cout << "Nodes visited: " << num_visited << std::endl;
+  // distance from source to source
+  dist[from] = 0;
 
-  return result;
+  // we store the distance additionally to the node, as to check for stale
+  // references later, as we might push some neighbors multiple times
+  std::vector<std::pair<size_t, float>> Q{{from, 0.0f}}; // <index, distance>
+
+  auto comp = [&](auto lhs, auto rhs){return lhs.second > rhs.second;};
+  std::make_heap(Q.begin(),Q.end(),comp);
+
+  // main loop
+  while (!Q.empty()) {
+      std::pop_heap(Q.begin(), Q.end(), comp);
+      auto [u, stored_distance] = Q.back();
+      Q.pop_back();
+
+      if (stored_distance != dist[u]) {
+          // this is a stale reference for u, indicating it was already
+          // processed with a better distance
+          continue;
+      }
+
+      if (u == to) {
+          // found the end! stop searching...
+          break;
+      }
+
+      Location location = adjacency_matrix[u];
+      for (size_t v = 0; v < adjacency_matrix.size(); ++v) {
+          float dist_between = location[v].value_or(std::numeric_limits<float>::infinity());
+          if (std::isinf(dist_between)) {
+              continue; // u and v are not neighbors
+          }
+
+          float alt = dist[u] + dist_between;
+          if (alt < dist[v]) {
+              dist[v] = alt;
+              previous[v] = u;
+              Q.push_back({v, alt});
+              std::push_heap(Q.begin(), Q.end(), comp);
+          }
+      }
+  }
+
+  // return path
+  std::vector<size_t> path;
+  size_t current = to;
+  while (true) {
+      if (current == undefined) {
+          return {};
+      }
+
+      path.push_back(current);
+      if (current == from) {
+          std::reverse(path.begin(), path.end());
+          return path;
+      }
+
+      current = previous[current];
+  }
 }
